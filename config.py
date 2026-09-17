01fd710aa77226c8fb8f7bf509ec6fc984079cc3
Arkodeep120611 <arkodeepkundu11@gmail.com>
2026-09-17 22:31:18 +0530

    Fix PostgreSQL driver for deployment

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 
 if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
     DATABASE_URL = DATABASE_URL.replace(
-        "postgres://", "postgresql+psycopg://", 1
+        "postgres://", "postgresql+psycopg2://", 1
     )
 
 
